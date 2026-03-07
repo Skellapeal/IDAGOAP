50efd49fa1a4001343504451654e7197569df790
Niall Ó Colmáin <ocolmain.niall.92@protonmail.com>
2026-03-07 16:01:27 +0000

    Added a new class for using gmotives to auto-select new goals
    No contents added. Just empty classes.

---
+++ goal_selector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ismis on 07/03/2026.
+//
+
+#include "goal_selector.h"
---
+++ goal_selector.h
@@ -0,0 +1,13 @@
+//
+// Created by ismis on 07/03/2026.
+//
+
+#ifndef IDAGOAP_GOAL_SELECTOR_H
+#define IDAGOAP_GOAL_SELECTOR_H
+
+
+class goal_selector {
+};
+
+
+#endif //IDAGOAP_GOAL_SELECTOR_H
